26a2e71179acd20530073e890e9612c9b5a40fef
Maximilian Helmersson <max.helmersson@gmail.com>
2020-10-13 10:00:25 +0200

    Arduino post test

--- routes/room.js
+++ routes/room.js
@@ -19,7 +19,7 @@ router.post('/', async(req, res) => {
     console.log("Req.body info");
     console.log(req.body);
     console.log(req.body[0]);
-    console.log(req.body[0].arduinoIdentifier);
+    //console.log(req.body[0].arduinoIdentifier);
     console.log(req.body.arduinoIdentifier);
 
     res.send(req.body.arduinoIdentifier);
